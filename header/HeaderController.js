1f1050c641c7330376c386d7e83488c5234f143c
Pat Dayton <daytonpe14@gmail.com>
2018-01-16 21:57:05 -0600

    update software/hardware

--- header/HeaderController.js
+++ header/HeaderController.js
@@ -12,7 +12,7 @@ const Header = require("./Header");
 
 router.get("/", function(req, res) {
   // const systemInfo = req.useragent;
-  console.log(req.headers["x-forwarded-for"]);
+  // console.log(req.headers["user-agent"]);
   let output = {};
   const ipaddress =
     req.headers["x-forwarded-for"] ||
@@ -21,21 +21,34 @@ router.get("/", function(req, res) {
     (req.connection.socket ? req.connection.socket.remoteAddress : null);
 
   output.ipaddress = ipaddress;
+  var software = req.headers["user-agent"].split(" ");
+  console.log(software);
+  output.software =
+    software[1].slice(1) +
+    " " +
+    software[2] +
+    " " +
+    software[3] +
+    " " +
+    software[4] +
+    " " +
+    software[6].slice(0, -1);
   output.language = req.headers["accept-language"].split(",")[0];
-  si.osInfo(function(data) {
-    // output.software = `${data.platform} ${data.distro}`;
-    output.software = data;
-
-    si.cpu(function(data) {
-      output.hardware = `${data.manufacturer} ${data.brand}`;
-
-      si.networkConnections(function(data) {
-        // console.log("NETWORK INTERFACES");
-        // console.log(data);
-        res.send(output);
-      });
-    });
-  });
+  res.send(output);
+  // si.osInfo(function(data) {
+  //   // output.software = `${data.platform} ${data.distro}`;
+  //   // output.software = data;
+  //
+  //   si.cpu(function(data) {
+  //     output.hardware = `${data.manufacturer} ${data.brand}`;
+  //
+  //     si.networkConnections(function(data) {
+  //       // console.log("NETWORK INTERFACES");
+  //       // console.log(data);
+  //       res.send(output);
+  //     });
+  //   });
+  // });
 });
 
 module.exports = router;
